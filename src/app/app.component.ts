1e5e65de32f486fb6bf150c520f792d62d51f8ab
Alex <oleksandr.kurmoiarov@smartexe.com>
2020-05-29 09:52:13 +0300

    Added owl-carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { images } from '../assets/data/images.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'swiper-for-angular';
+export class AppComponent implements OnInit {
+  public images = images;
+  
+  customOptions: OwlOptions = {
+    // loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      }
+    },
+    nav: false
+  }
+  
+  ngOnInit () {
+    
+  }
 }
